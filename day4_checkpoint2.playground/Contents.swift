041e9651212e6a20e894261547c76ad71335a115
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-20 13:15:18 +0100

    day4_checkpoint2

---
+++ day4_checkpoint2.playground/Contents.swift
@@ -0,0 +1,5 @@
+
+var cities = ["Cracow", "London", "Oslo", "New York", "Oslo", "London"]
+var uniqueCities = Set(cities)
+print("cities array has \(cities.count) items, and unique are \(uniqueCities.count) of them ")
+
