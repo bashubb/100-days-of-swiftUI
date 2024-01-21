2a05526ae7e078768d20e55ecd94b59abe3df1ae
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-21 22:10:37 +0100

    day11_checkpoint6

--- day11_checkpoint6.playground/Contents.swift
+++ day11_checkpoint6.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
 
-var greeting = "Hello, playground"
+struct Car {
+    let model: String
+    let numberOfSeats: Int
+    
+    private(set) var currentGear = 1 {
+        didSet {
+            print("You've changed gear from \(oldValue) to \(currentGear)")
+        }
+    }
+    
+    init(model: String, numberOfSeats: Int) {
+        self.model = model
+        self.numberOfSeats = numberOfSeats
+    }
+    
+    mutating func changeGear(_ command: String) {
+        if command.lowercased() == "up" && currentGear < 10 {
+            currentGear += 1
+        } else if command.lowercased() == "down" && currentGear > 1 {
+            currentGear -= 1
+        } else {
+            print("You're bad driver")
+        }
+    }
+}
+
+var newCar = Car(model: "Honda", numberOfSeats: 5)
+newCar.changeGear("up")         // prints  You've changed gear from 1 to 2
+newCar.changeGear("down")       // prints  You've changed gear from 2 to 1
+newCar.changeGear("down")       // prints  You're bad driver
+newCar
