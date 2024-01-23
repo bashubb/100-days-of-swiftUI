da90c49da6ef1e1dd5d3b47f753e90e51b6b176b
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-23 09:36:58 +0100

    We Split: Changes according to challange1, challange2, challange3

--- Project1/WeSplit/WeSplit/ContentView.swift
+++ Project1/WeSplit/WeSplit/ContentView.swift
@@ -13,16 +13,17 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
     
+    // Commented because of Challange 3
     let tipPercentages = [10, 15, 20, 25, 0]
-    var totalPerPerson: Double {
+    var totalAmount: (perPerson:Double, grandTotal: Double) {
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
         
-        let totalWithTip =  checkAmount + (checkAmount * tipSelection / 100)
+        let grandTotal =  checkAmount + (checkAmount * tipSelection / 100)
         
-        let totalPerPerson  = totalWithTip / peopleCount
+        let totalPerPerson  = grandTotal / peopleCount
         
-        return totalPerPerson
+        return (totalPerPerson, grandTotal)
     }
    
     var body: some View {
@@ -43,15 +44,30 @@ struct ContentView: View {
                 
                 Section("How much do you want to tip ") {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        
+                        /* commented because of challange 3
+                            ForEach(tipPercentages, id: \.self) {
+                                Text($0, format: .percent)
+                            }
+                         }
+                         .pickerStyle(.segmented)
+                         */
+                        
+                        ForEach(0..<101) {
                             Text($0, format: .percent)
                         }
-                    }
-                    .pickerStyle(.segmented)
+                    }.pickerStyle(.navigationLink)
                 }
                 
-                Section {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                // Challenge 1 - header "Amount per person"
+                Section("Amount per person") {
+                    Text(totalAmount.perPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                
+                //Challenge 2 - 4th Section "Grand Total"
+                Section("Grand Total") {
+                    Text(totalAmount.grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
             .navigationTitle("WeSplit")
