7f59a65f6c4bbb1df47011ed84738e39095bea3a
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-23 11:57:37 +0100

    Unit Converter: Challange 1 : Unit Converter - second Solution

--- Challange1/UnitConverter/UnitConverter/ContentView.swift
+++ Challange1/UnitConverter/UnitConverter/ContentView.swift
@@ -10,61 +10,41 @@ import SwiftUI
 struct ContentView: View {
     
     @State var inputValue = 0.0
-    @State var inputUnit = "Celcius"
-    @State var outputUnit = "Celcius"
+    @State var inputUnit: Dimension = UnitTemperature.celsius
+    @State var outputUnit: Dimension = UnitTemperature.fahrenheit
     @State var choosenConvertion = "Temperature"
     @FocusState var isFocused : Bool
     
     let conversions = ["Temperature", "Lenght", "Time", "Volume"]
     
-    let temperatureUnits = [
-        "Celcius" : UnitTemperature.celsius,
-        "Farenheit" : UnitTemperature.fahrenheit,
-        "Kelvin" : UnitTemperature.kelvin]
-    
-    let lenghtUnits = [
-        "Meters" : UnitLength.meters,
-        "Kilometers" : UnitLength.kilometers,
-        "Feet" : UnitLength.feet,
-        "Yards" : UnitLength.yards,
-        "Miles" : UnitLength.miles]
-    
-    let timeUnits = [
-        "Seconds" : UnitDuration.seconds,
-        "Minutes" : UnitDuration.minutes,
-        "Hours" : UnitDuration.hours]
-    
-    let volumeUnits = [
-        "Mililiters" : UnitVolume.milliliters,
-        "Liters" : UnitVolume.liters,
-        "Cups" : UnitVolume.cups,
-        "Pints" : UnitVolume.pints,
-        "Gallons" : UnitVolume.gallons]
+    let units = [
+        [UnitTemperature.celsius,UnitTemperature.fahrenheit,UnitTemperature.kelvin],
+        [UnitLength.meters,UnitLength.kilometers,UnitLength.feet, UnitLength.yards, UnitLength.miles],
+        [UnitDuration.seconds, UnitDuration.minutes, UnitDuration.hours],
+        [UnitVolume.milliliters, UnitVolume.liters, UnitVolume.cups, UnitVolume.pints,UnitVolume.gallons]
+    ]
+    let formatter: MeasurementFormatter
     
     
-    var unitsForConversion: [String:Dimension] {
-        if choosenConvertion == "Temperature" {
-            return temperatureUnits
-        } else if choosenConvertion == "Lenght" {
-            return lenghtUnits
-        } else if choosenConvertion == "Time" {
-            return timeUnits
-        } else if choosenConvertion == "Volume" {
-            return volumeUnits
+    var unitsForConversion: [Dimension] {
+        let unitIndex = conversions.firstIndex(of: choosenConvertion)
+        guard let unitIndex = unitIndex else{
+            return units[0]
         }
-        return temperatureUnits
+        return units[unitIndex]
     }
     
-    var outputValue: String? {
-        let input = Measurement(value: inputValue, unit: unitsForConversion[inputUnit] ?? UnitTemperature.celsius)
-        let output = input.converted(to: unitsForConversion[outputUnit] ?? UnitTemperature.celsius).formatted()
+    var result: String {
+        let inputMeasurement = Measurement(value: inputValue, unit: inputUnit)
+        let outputMeasurement = inputMeasurement.converted(to: outputUnit)
         
-        return "\(output)"
+        return formatter.string(from: outputMeasurement)
     }
     
     var body: some View {
         NavigationStack {
             Form {
+              
                 Section("choose convertion") {
                     Picker("Choose convertion", selection: $choosenConvertion) {
                         ForEach(conversions, id: \.self) {
@@ -74,19 +54,21 @@ struct ContentView: View {
                     .pickerStyle(.segmented)
                 }
                 
+                
                 Section("choose Units") {
                     Picker("Input Unit", selection: $inputUnit){
-                        ForEach(Array(unitsForConversion.keys), id: \.self) { name in
-                            Text(name)
+                        ForEach(unitsForConversion, id: \.self) {
+                            Text(formatter.string(from: $0).capitalized)
                         }
                     }
                     Picker("Output Unit", selection: $outputUnit){
-                        ForEach(Array(unitsForConversion.keys), id: \.self) { name in
-                            Text(name)
+                        ForEach(unitsForConversion, id: \.self) {
+                            Text(formatter.string(from: $0).capitalized)
                         }
                     }
                     
                 }
+                
                 
                 Section("Input") {
                     TextField("Input Value", value: $inputValue, format: .number)
@@ -94,9 +76,13 @@ struct ContentView: View {
                         .focused($isFocused)
                 }
                 
-                Section("Result") {
-                    Text(outputValue ?? "")
+                Section("result") {
+                    Text(result)
                 }
+                
+                
+                
+                
                 
             }
             .navigationTitle("Unit Converter")
@@ -110,8 +96,14 @@ struct ContentView: View {
                 }
             }
             
-        
+            
         }
+    }
+    
+    init() {
+        formatter = MeasurementFormatter()
+        formatter.unitOptions = .providedUnit
+        formatter.unitStyle = .long
     }
 }
 
