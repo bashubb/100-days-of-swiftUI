a0d31c331ce05ed49e168b07abe795eec0219f71
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-23 09:43:52 +0100

    GuessTheFlag: initial commit

---
+++ Project2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by HubertMac on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
