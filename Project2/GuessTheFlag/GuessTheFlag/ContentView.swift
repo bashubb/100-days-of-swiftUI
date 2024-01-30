200c69b5943aceff24f4e999d2ef1b1e54f91704
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-30 13:03:19 +0100

    Styling

--- Project2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -16,21 +16,26 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
+                    
                     Text(countries[correctAnswer])
                         .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
                 ForEach(0..<3) { number in
                     Button {
                       flagTapped(number)
                     } label: {
                         Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)
                     }
                     
                 }
