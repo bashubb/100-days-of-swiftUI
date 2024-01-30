bf3f6b77d1c1400bcafb6bf8a0553310bd95fb17
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-30 15:02:58 +0100

    Initial commit

---
+++ Project3/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by HubertMac on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
