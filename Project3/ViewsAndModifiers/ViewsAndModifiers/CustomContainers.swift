2ec2a50b31d644a1e9d3666677c789af6ce3db7e
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-30 21:20:23 +0100

    Custom Containers

---
+++ Project3/ViewsAndModifiers/ViewsAndModifiers/CustomContainers.swift
@@ -0,0 +1,39 @@
+//
+//  CustomContainers.swift
+//  ViewsAndModifiers
+//
+//  Created by HubertMac on 30/01/2024.
+//
+
+import SwiftUI
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CustomContainers: View {
+    var body: some View {
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+        }
+    }
+}
+
+#Preview {
+    CustomContainers()
+}
