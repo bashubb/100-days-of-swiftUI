35870f8bcc6eec61f8d271177a6ccc558929f9b2
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-31 09:39:54 +0100

    Project3 challenge 1 - conditional modifier

---
+++ Project3/challenge1/Project3Challenge1/Project3Challenge1/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by HubertMac on 22/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    var totalAmount: (perPerson:Double, grandTotal: Double) {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let grandTotal =  checkAmount + (checkAmount * tipSelection / 100)
+        
+        let totalPerPerson  = grandTotal / peopleCount
+        
+        return (totalPerPerson, grandTotal)
+    }
+   
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .focused($amountIsFocused)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section("How much do you want to tip ") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                
+                // Challenge 1 conditional modifier
+                Section("Amount per person") {
+                    Text(totalAmount.perPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(tipPercentage == 0 ? .red : .primary)
+                }
+                
+                // Challenge 1 conditional modifier
+                Section("Grand Total") {
+                    Text(totalAmount.grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(tipPercentage == 0 ? .red : .primary)
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project3/challenge1/Project3Challenge1/Project3Challenge1/Project3Challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project3Challenge1App.swift
+//  Project3Challenge1
+//
+//  Created by HubertMac on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Project3Challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
