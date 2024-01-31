2d330640ec5e83ef13fe9b803db0a3e5849ebee6
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-31 11:14:57 +0100

    initial_commit MilestoneProjects1-3

---
+++ Milestone-Projects1-3/Milestone-Projects1-3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Milestone-Projects1-3
+//
+//  Created by HubertMac on 31/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Milestone-Projects1-3/Milestone-Projects1-3/Milestone_Projects1_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone_Projects1_3App.swift
+//  Milestone-Projects1-3
+//
+//  Created by HubertMac on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Milestone_Projects1_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
