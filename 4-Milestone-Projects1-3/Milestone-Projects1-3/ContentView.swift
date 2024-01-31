7ff7f6a4e6dbe7181c71399dd6e66bf514a16f44
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-31 12:40:21 +0100

    Rock Paper Scissors game

--- 4-Milestone-Projects1-3/Milestone-Projects1-3/ContentView.swift
+++ 4-Milestone-Projects1-3/Milestone-Projects1-3/ContentView.swift
@@ -8,14 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    let moves = ["✊", "🫲", "✌️"]
+    
+    @State private var computerChoice = Int.random(in: 0..<3)
+    @State private var shouldWin = Bool.random()
+    
+    @State private var score = 0
+    @State private var questionCount = 1
+    @State private var showingResults = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text("Computer has played...")
+                .font(.headline)
+            
+            Text(moves[computerChoice])
+                .font(.system(size:200))
+                
+            if shouldWin {
+                Text("Which one wins?")
+                    .foregroundStyle(.green)
+                    .font(.title)
+            } else {
+                Text("Wich one loses?")
+                    .foregroundStyle(.red)
+                    .font(.title)
+            }
+            
+            HStack {
+                ForEach(0..<3) {number in
+                    Button(moves[number]) {
+                        play(choice: number)
+                    }
+                    .font(.system(size: 80))
+                }
+            }
+            
+            Spacer()
+            
+            Text("Score: \(score)")
+                .font(.subheadline)
+            
+            Spacer()
         }
-        .padding()
+        .alert("Game over", isPresented: $showingResults) {
+            Button("Play again", action: reset)
+        } message: {
+            Text("Your score was \(score)")
+        }
+        
+    }
+    
+    func play(choice: Int) {
+        let winningMoves = [1, 2, 0]
+        let didWin: Bool
+        
+        if shouldWin {
+            didWin = choice == winningMoves[computerChoice]
+        } else {
+            didWin = winningMoves[choice] == computerChoice
+        }
+        
+        if didWin {
+            score += 1
+        } else {
+            score -= 1
+        }
+        
+        if questionCount == 10 {
+            showingResults = true
+        } else {
+            computerChoice = Int.random(in: 0..<3)
+            shouldWin = Bool.random()
+            questionCount += 1
+        }
+    }
+    
+    func reset() {
+        computerChoice = Int.random(in: 0..<3)
+        shouldWin = Bool.random()
+        questionCount = 1
+        score = 0
     }
 }
 
