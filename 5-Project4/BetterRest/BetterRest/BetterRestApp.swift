701d0d354e66ecab692d1912636e8b5cbaae29e6
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-02-11 22:19:38 +0100

    initial_commit BetterRest

---
+++ 5-Project4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by HubertMac on 11/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
