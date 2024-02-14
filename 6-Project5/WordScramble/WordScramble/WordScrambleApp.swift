0e46498077a5ce4a623c48917434ffa1b9ddbcde
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-02-14 13:38:26 +0100

    initial commit

---
+++ 6-Project5/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by HubertMac on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
