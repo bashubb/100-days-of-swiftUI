b11de26e2499b281e99cff0b758cc56dc0c019ef
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-02-21 15:49:52 +0100

    challenges:
    When you tap a flag, make it spin around 360 degrees on the Y axis.
    Make the other two buttons fade out to 25% opacity.
    Add a third effect of your choosing to the two flags the user didn’t choose – maybe make them scale down? Or flip in a different direction? Experiment!

---
+++ 7-Project6/challenges/Challenge6/Challenge6/Challenge6App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge6App.swift
+//  Challenge6
+//
+//  Created by HubertMac on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Challenge6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
