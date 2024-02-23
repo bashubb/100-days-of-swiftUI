faae6afd9b9bbc3c4bcaecea11420b8903b45fb8
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-02-23 10:47:40 +0100

    initial_commit iExpense

---
+++ 8-Milestone-Projects4-6/MilestoneProjects4-6/MilestoneProjects4-6/MilestoneProjects4_6App.swift
@@ -0,0 +1,17 @@
+//
+//  MilestoneProjects4_6App.swift
+//  MilestoneProjects4-6
+//
+//  Created by HubertMac on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MilestoneProjects4_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
