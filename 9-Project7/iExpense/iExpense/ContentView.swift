68aa1a7dcb20f17ebaa56ca94d8b4401be778314
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-02-23 12:07:28 +0100

    Building a list it can be delete form struct ExpenseItem - data model observabe class Expenses - for managing data flow ContentView: List with rows which can be deleted by swiping - .onDelete modifier and removeItems method

--- 9-Project7/iExpense/iExpense/ContentView.swift
+++ 9-Project7/iExpense/iExpense/ContentView.swift
@@ -5,17 +5,43 @@
 //  Created by HubertMac on 23/02/2024.
 //
 
+
 import SwiftUI
 
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    @State private var expences = Expenses()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(expences.items, id:\.name) {item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expences.items.append(expense)
+                }
+            }
         }
-        .padding()
+    }
+    
+    func removeItems(at offset: IndexSet) {
+        expences.items.remove(atOffsets: offset)
     }
 }
 
