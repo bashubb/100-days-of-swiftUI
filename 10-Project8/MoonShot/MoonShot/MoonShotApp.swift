cca147583b13e5f770d93aaab41e229b44c778c7
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-02-26 12:37:31 +0100

    initial_commit MoonShot

---
+++ 10-Project8/MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by HubertMac on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
