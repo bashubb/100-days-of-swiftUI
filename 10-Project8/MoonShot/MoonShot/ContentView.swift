cd6dca9885a39551fb4b08a056002265e2e579e7
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-02-28 11:46:26 +0100

    Loading a specific kind of Codable data

--- 10-Project8/MoonShot/MoonShot/ContentView.swift
+++ 10-Project8/MoonShot/MoonShot/ContentView.swift
@@ -8,14 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts = Bundle.main.decode("astronauts.json")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(String(astronauts.count))
     }
 }
 
