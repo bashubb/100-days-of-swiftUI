33baf3e2a17a32b99c71a4725573f04a69095728
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-03-05 15:02:19 +0100

    initial_commit CupcakeCorner

---
+++ 13-Project10/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by HubertMac on 05/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
