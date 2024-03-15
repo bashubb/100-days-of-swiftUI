9ce1aaf71cb84a2ff19d48224b9d9d5528c91ba2
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-03-15 20:03:33 +0100

    Adding a custom star rating component

--- 14-Project11/Bookworm/Bookworm/AddBookView.swift
+++ 14-Project11/Bookworm/Bookworm/AddBookView.swift
@@ -37,11 +37,7 @@ struct AddBookView: View {
                 Section("Write a review") {
                     TextEditor(text: $review)
                     
-                    Picker("Rating", selection: $rating) {
-                        ForEach(0..<6){
-                            Text(String($0))
-                        }
-                    }
+                    RatingView(rating: $rating)
                 }
                 
                 Section{
