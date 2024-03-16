a700dba5598e0ec5af6174646c154f225b036233
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-03-16 13:16:23 +0100

    Sorting SwiftData queries using Sort Descriptor

--- 14-Project11/Bookworm/Bookworm/ContentView.swift
+++ 14-Project11/Bookworm/Bookworm/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
+    @Query(sort: [SortDescriptor(\Book.title),
+                  SortDescriptor(\Book.author)
+                 ]) var books: [Book]
     
     @State private var showingAddScreen = false
     
