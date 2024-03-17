a834e36e380fcf1e53628ef8fd4175e20cc5ae73
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-03-17 06:30:26 +0100

    Deleting from a SwiftData Query

--- 14-Project11/Bookworm/Bookworm/ContentView.swift
+++ 14-Project11/Bookworm/Bookworm/ContentView.swift
@@ -34,12 +34,17 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .navigationTitle("Bookworm")
             .navigationDestination(for: Book.self, destination: { book in
                 DetailView(book: book)
             })
             .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add Book", systemImage: "plus") {
                         showingAddScreen.toggle()
@@ -49,9 +54,14 @@ struct ContentView: View {
             .sheet(isPresented: $showingAddScreen){
                 AddBookView()
             }
-            
         }
-        
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            modelContext.delete(book)
+        }
     }
 }
 
