34ed53c10e60b796f3d5b3f2727b1d38c7a69104
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-03-19 22:55:12 +0100

    initial_commit Milestone10-12

---
+++ 16-Milestone-Project10-12/Milestone10-12/Milestone10-12/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Milestone10-12
+//
+//  Created by HubertMac on 19/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 16-Milestone-Project10-12/Milestone10-12/Milestone10-12/Milestone10_12App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone10_12App.swift
+//  Milestone10-12
+//
+//  Created by HubertMac on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Milestone10_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
