fc7dc9b26b222def6cd8481c621fb9075c113780
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-03-29 09:56:56 +0100

    Syncing SwiftData with CloudKit

--- 15-Project12/SwiftDataProject/SwiftDataProject/Dynamically sorting and filtering/UsersView.swift
+++ 15-Project12/SwiftDataProject/SwiftDataProject/Dynamically sorting and filtering/UsersView.swift
@@ -18,7 +18,7 @@ struct UsersView: View {
                 
                 Spacer()
                 
-                Text(String(user.jobs.count))
+                Text(String(user.unwrappedJobs.count))
                     .fontWeight(.black)
                     .padding(.horizontal, 10)
                     .padding(.vertical, 5)
@@ -43,8 +43,8 @@ struct UsersView: View {
         
         modelContext.insert(user1)
         
-        user1.jobs.append(job1)
-        user1.jobs.append(job2)
+        user1.jobs?.append(job1)
+        user1.jobs?.append(job2)
     }
 }
 
