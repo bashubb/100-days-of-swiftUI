bd2a9551e00fa64a468e169d36834cf25908e68a
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-04-04 22:39:46 +0200

    initial_commit Insta Filter

---
+++ 17-Project13/InstaFilter/InstaFilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaFilter
+//
+//  Created by HubertMac on 30/03/2024.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 17-Project13/InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by HubertMac on 30/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
