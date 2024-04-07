ee1181886c668bdff06c8862c9541051dea60971
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-04-07 20:11:33 +0200

    Bulding basic UI

--- 17-Project13/InstaFilter/InstaFilter/ContentView.swift
+++ 17-Project13/InstaFilter/InstaFilter/ContentView.swift
@@ -5,13 +5,45 @@
 //  Created by HubertMac on 30/03/2024.
 //
 
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-        VStack {
-            
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+                
+                Spacer()
+                
+                HStack{
+                    Text("Intensity")
+                    Slider(value:$filterIntensity)
+                }
+                
+                HStack{
+                    Button("Change Filter") {
+                        //change filter
+                    }
+                    
+                    Spacer()
+                    
+                    // share the picture
+                }
+            }
+            .padding([.horizontal, .bottom])
+        .navigationTitle("Instafilter")
         }
     }
 }
