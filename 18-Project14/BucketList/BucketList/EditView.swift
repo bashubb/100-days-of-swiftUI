37299d0df30f22a23fddc116cb6352bedf4ee351
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-04-10 13:44:07 +0200

    Sorting wikipedia results

--- 18-Project14/BucketList/BucketList/EditView.swift
+++ 18-Project14/BucketList/BucketList/EditView.swift
@@ -40,7 +40,7 @@ struct EditView: View {
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ") +
-                            Text("Page description here")
+                            Text(page.description)
                                 .italic()
                         }
                         
@@ -86,7 +86,7 @@ struct EditView: View {
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
             let items = try JSONDecoder().decode(Result.self, from: data)
-            pages = items.query.pages.values.sorted { $0.title < $1.title}
+            pages = items.query.pages.values.sorted()
             loadingState = .loaded
         } catch {
             loadingState = .failed
