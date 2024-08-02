8271698b3f9a9838d448a25df2e62db1c02c1bc8
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-08-02 13:14:25 +0200

    initial_comit

---
+++ 23-Project18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by HubertMac on 27/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+sasaS
+#Preview {
+    ContentView()
+}
---
+++ 23-Project18/LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by HubertMac on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
