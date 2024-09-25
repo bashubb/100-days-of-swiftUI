a94dc36430614d5fda9d7e91758189ab1de647d4
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-09-25 22:10:32 +0200

    absolute p[ositioning for SwiftUI views

--- 23-Project18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 23-Project18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -12,38 +12,11 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
-
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostaws")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                Image(systemName: "circle")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-            
-            VStack {
-                Text("Full name: ")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hello world!")
+            .background(.red)
+            .offset(x: 100, y: 100)
     }
 }
 
