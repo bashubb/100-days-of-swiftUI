3afe7bb59f9209f5bc237d9f44eb15fe0067e5f4
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-09-25 21:36:33 +0200

    line alignment

--- 23-Project18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 23-Project18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -14,9 +14,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world")
-            .padding(20)
-            .background(.red)
+        HStack(alignment: .lastTextBaseline) {
+            Text("Live")
+                .font(.caption)
+            
+            Text("long")
+            
+            Text("and")
+                .font(.title)
+            
+            Text("prosper")
+                .font(.largeTitle)
+        }
     }
 }
 
