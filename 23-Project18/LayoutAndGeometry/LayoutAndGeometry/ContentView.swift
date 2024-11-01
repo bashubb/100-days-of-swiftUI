07a3a4dbc5615854c4ed55c28f0d3255eebfbd0e
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-11-01 23:23:29 +0100

    scroll effects using geometryReader1

--- 23-Project18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 23-Project18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -1,60 +1,27 @@
-//
-//  ContentView.swift
-//  LayoutAndGeometry
-//
-//  Created by HubertMac on 27/06/2024.
-//
-
-/*
- 1. A parent view proposes a size for its child.
- 2. Based on that information, the child then chooses its own size and the parent must respect that choice.
- 3. The parent then positions the child in its coordinate space.*/
-
 import SwiftUI
 
-struct OuterView: View {
-    var body: some View {
-        VStack {
-            Text("top")
-            
-            InnerView()
-                .background(.green)
-            
-            Text("bottom")
-        }
-    }
-}
-
-struct InnerView: View {
-    var body: some View {
-        HStack {
-            Text("Left")
-            
-            GeometryReader { proxy in
-                Text("Centre")
-                    .background(.blue)
-                    .onTapGesture {
-                        print("Global center: \(proxy.frame(in: .global).midX) x \(proxy.frame(in: .global).midY)")
-                        print("Custom center: \(proxy.frame(in: .named("custom")).midX) x \(proxy.frame(in: .named("custom")).midY)")
-                        print("Local center: \(proxy.frame(in: .local).midX) x \(proxy.frame(in: .local).midY)")
-                    }
-                              
-            }
-            .background(.orange)
-            
-            Text("Right")
-        }
-    }
-}
-
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink]
     var body: some View {
-        OuterView()
-            .background(.red)
-            .coordinateSpace(name: "custom")
-        
+        GeometryReader { fullView in        ScrollView {
+            ForEach(0..<50) { index in
+                GeometryReader { proxy in
+                    Text("Row #\(index)")
+                        .font(.title)
+                        .frame(maxWidth: .infinity)
+                        .background(colors[index % 5])
+                        .rotation3DEffect(
+                            .degrees(proxy.frame(in: .global).minY  - fullView.size.height / 2) / 5,
+                            axis: /*@START_MENU_TOKEN@*/(x: 0.0, y: 1.0, z: 0.0)/*@END_MENU_TOKEN@*/
+                        )
+                }
+                .frame(height: 40)
+            }
+        }
+        }
     }
 }
+
 
 #Preview {
     ContentView()
