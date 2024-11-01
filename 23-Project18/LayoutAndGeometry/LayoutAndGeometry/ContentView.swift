65fd1a0c551ba0a40713dc817465be1372203dbb
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-11-01 23:29:15 +0100

    scrollView effects using geometryReader

--- 23-Project18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 23-Project18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -3,21 +3,23 @@ import SwiftUI
 struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink]
     var body: some View {
-        GeometryReader { fullView in        ScrollView {
-            ForEach(0..<50) { index in
-                GeometryReader { proxy in
-                    Text("Row #\(index)")
-                        .font(.title)
-                        .frame(maxWidth: .infinity)
-                        .background(colors[index % 5])
-                        .rotation3DEffect(
-                            .degrees(proxy.frame(in: .global).minY  - fullView.size.height / 2) / 5,
-                            axis: /*@START_MENU_TOKEN@*/(x: 0.0, y: 1.0, z: 0.0)/*@END_MENU_TOKEN@*/
-                        )
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { number in
+                    GeometryReader { proxy in
+                        Text("Number \(number)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .rotation3DEffect(
+                                .degrees(-proxy.frame(in: .global).minX / 8),
+                                      axis: /*@START_MENU_TOKEN@*/(x: 0.0, y: 1.0, z: 0.0)/*@END_MENU_TOKEN@*/
+                            )
+                            .frame(width: 200, height: 200)
+                    }
+                    .frame(width: 200, height: 200)
                 }
-                .frame(height: 40)
             }
-        }
         }
     }
 }
